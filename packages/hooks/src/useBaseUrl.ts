f6eb81059c70e0fc93f4caddb3ca366e66339dc5
Taylor Bantle <taylor@dolthub.com>
2024-01-16 10:41:40 -0800

    Naming, publish script

--- packages/hooks/src/useBaseUrl.ts
+++ packages/hooks/src/useBaseUrl.ts
@@ -3,17 +3,17 @@ import { useEffect, useState } from "react";
 // getUrlFn is a function that returns the base url based on location.hostname.
 export default function useBaseUrl(
   getUrlFn: () => string,
-  defaultHost = "",
+  defaultUrl = "",
 ): string {
-  const [host, setHost] = useState(defaultHost);
+  const [url, setUrl] = useState(defaultUrl);
   const [setOnce, setSetOnce] = useState(false);
 
   useEffect(() => {
     if (typeof window !== "undefined" && !setOnce) {
-      setHost(getUrlFn());
+      setUrl(getUrlFn());
       setSetOnce(true);
     }
-  }, [host, setHost, getUrlFn]);
+  }, [url, setUrl, getUrlFn]);
 
-  return host;
+  return url;
 }
