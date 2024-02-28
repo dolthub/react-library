b4adf52f82e25beabee8dfabc3e2e23b1a7df913
Taylor Bantle <taylor@dolthub.com>
2024-02-27 16:37:28 -0800

    components: Clean up colors

--- packages/components/src/tailwind/theme/hosted/colors.ts
+++ packages/components/src/tailwind/theme/hosted/colors.ts
@@ -1,29 +1,10 @@
 export const colors = {
-  // "ld-darkerblue": "#1e2842",
   "ld-darkblue": "#183362",
   "ld-mediumblue": "#2b5db6",
-  // "ld-blue": "#6db0fc",
   "ld-bluegrey": "#284f94",
   "ld-grey": "#f2f5fb",
   "ld-brightgreen": "#29e3c1",
   "ld-orange": "#ff820f",
-
-  // Database page colors
-  // "ld-darkestblue": "#182134",
-  // "ld-green": "#5deda2",
-  // "ld-pink": "#fc42c9", // not used
-  // "acc-hoverlinkblue": "#3d91f0",
-  // "acc-hoverblue": "#1d2c7f",
-  // "acc-linkblue": "#1f6dc6",
-  // "acc-darkgrey": "#5d6280",
-  // "acc-hovergreen": "#6fdda4",
-  // "acc-lightgreen": "#d4f5e4",
-  // "acc-hoverred": "#fca8a7",
-  // "acc-code": "#575662",
-  // "acc-orange": "#ed8936",
-  // "acc-pink": "#d588d5",
-  // "acc-bright-green": "#29e3c1",
-  // "acc-purple": "#805edd",
 };
 
 export const tailwindColorTheme = {
