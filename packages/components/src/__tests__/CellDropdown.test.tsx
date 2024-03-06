47d91cdf3601b1ce0d96a4a4d05c6da6a1b0dfde
Taylor Bantle <taylor@dolthub.com>
2024-03-06 12:40:52 -0800

    components: Fixes

--- packages/components/src/__tests__/CellDropdown.test.tsx
+++ packages/components/src/__tests__/CellDropdown.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import CellDropdown from "../CellDropdown";
+import { setup } from "./testUtils.test";
 
 describe("test CellDropdown", () => {
   it("renders without crashing", () => {
@@ -16,68 +17,68 @@ describe("test CellDropdown", () => {
     expect(screen.getByRole("button")).toBeInTheDocument();
   });
 
-  // it("toggles dropdown visibility on button click", async () => {
-  //   const setShowDropdown = jest.fn();
-  //   render(
-  //     <CellDropdown
-  //       showDropdown={false}
-  //       setShowDropdown={setShowDropdown}
-  //       buttonClassName=""
-  //     >
-  //       <div>Dropdown Content</div>
-  //     </CellDropdown>,
-  //   );
+  it("toggles dropdown visibility on button click", async () => {
+    const setShowDropdown = jest.fn();
+    const { user } = setup(
+      <CellDropdown
+        showDropdown={false}
+        setShowDropdown={setShowDropdown}
+        buttonClassName=""
+      >
+        <div>Dropdown Content</div>
+      </CellDropdown>,
+    );
 
-  //   const button = screen.getByRole("button");
-  //   await userEvent.click(button);
-  //   expect(setShowDropdown).toHaveBeenCalledWith(true);
-  // });
+    const button = screen.getByRole("button");
+    await user.click(button);
+    expect(setShowDropdown).toHaveBeenCalledWith(true);
+  });
 
-  // it("closes dropdown when clicking outside", async () => {
-  //   const setShowDropdown = jest.fn();
-  //   render(
-  //     <div>
-  //       <CellDropdown
-  //         showDropdown={true}
-  //         setShowDropdown={setShowDropdown}
-  //         buttonClassName=""
-  //       >
-  //         <div>Dropdown Content</div>
-  //       </CellDropdown>
-  //       <div data-testid="outside">Outside Area</div>
-  //     </div>,
-  //   );
+  it("closes dropdown when clicking outside", async () => {
+    const setShowDropdown = jest.fn();
+    const { user } = setup(
+      <div>
+        <CellDropdown
+          showDropdown={true}
+          setShowDropdown={setShowDropdown}
+          buttonClassName=""
+        >
+          <div>Dropdown Content</div>
+        </CellDropdown>
+        <div data-testid="outside">Outside Area</div>
+      </div>,
+    );
 
-  //   const outsideArea = screen.getByTestId("outside");
-  //   await userEvent.click(outsideArea);
-  //   expect(setShowDropdown).toHaveBeenCalledWith(false);
-  // });
+    const outsideArea = screen.getByTestId("outside");
+    await user.click(outsideArea);
+    expect(setShowDropdown).toHaveBeenCalledWith(false);
+  });
 
-  // it("applies correct class based on `forRow` prop", () => {
-  //   const { rerender } = render(
-  //     <CellDropdown
-  //       showDropdown={false}
-  //       setShowDropdown={() => {}}
-  //       buttonClassName=""
-  //       forRow={false}
-  //     >
-  //       <div>Dropdown Content</div>
-  //     </CellDropdown>,
-  //   );
+  it("applies correct class based on `forRow` prop", () => {
+    const { rerender } = render(
+      <CellDropdown
+        showDropdown={false}
+        setShowDropdown={() => {}}
+        buttonClassName=""
+        forRow={false}
+      >
+        <div>Dropdown Content</div>
+      </CellDropdown>,
+    );
 
-  //   expect(screen.getByRole("button")).not.toHaveClass("rowButton"); // Adapt class names as necessary
+    expect(screen.getByRole("button")).not.toHaveClass(/rowButton/);
 
-  //   rerender(
-  //     <CellDropdown
-  //       showDropdown={false}
-  //       setShowDropdown={() => {}}
-  //       buttonClassName=""
-  //       forRow={true}
-  //     >
-  //       <div>Dropdown Content</div>
-  //     </CellDropdown>,
-  //   );
+    rerender(
+      <CellDropdown
+        showDropdown={false}
+        setShowDropdown={() => {}}
+        buttonClassName=""
+        forRow
+      >
+        <div>Dropdown Content</div>
+      </CellDropdown>,
+    );
 
-  //   expect(screen.getByRole("button")).toHaveClass("rowButton"); // Adapt class names as necessary
-  // });
+    expect(screen.getByRole("button")).toHaveClass(/rowButton/);
+  });
 });
