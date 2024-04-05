c611f9b2487efb643359def25edc4ba599f092ec
Taylor Bantle <taylor@dolthub.com>
2024-04-05 09:04:05 -0700

    components: Remove storybook css

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -16,7 +16,6 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("@storybook/addon-styling-webpack"),
-    getAbsolutePath("display-element-css"),
     {
       name: getAbsolutePath("storybook-css-modules"),
       options: {
