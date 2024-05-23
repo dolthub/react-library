496dcc7fc8da8fb5a05ae9763c7d6b967690d0fe
hagiwara <tamalign@gmail.com>
2024-05-23 09:27:22 +0900

    Make excerpt function compatible with multibyte characters

--- packages/utils/src/excerpt.ts
+++ packages/utils/src/excerpt.ts
@@ -1,6 +1,25 @@
-const truncate = (str: string, len: number): string =>
-  `${str.slice(0, len - 1)}…`;
+// Half-width characters have a width of 1, while full-width characters, like those in Japanese and Chinese, have a width of 2.
+function getWidth(char: string): number {
+
+  return char.match(/[ -~]/) ? 1 : 2;
+}
+
+function truncate(str: string, len: number): string {
+  let truncated = '';
+  let width = 0;
+
+  for (let char of str) {
+    width += getWidth(char);
+    if (width > len - 1) {
+      return `${truncated}…`;
+    }
+    truncated += char;
+  }
+
+  return truncated;
+}
 
 export default function excerpt(str: string, len = 100): string {
-  return str.length < len ? str : truncate(str, len);
+  const width = Array.from(str).reduce((sum, char) => sum + getWidth(char), 0);
+  return width <= len ? str : truncate(str, len);
 }
