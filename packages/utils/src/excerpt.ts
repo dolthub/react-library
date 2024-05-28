af7230e61b6cdd1488702e82917c89461efe002e
Taylor Bantle <taylor@dolthub.com>
2024-05-28 12:46:29 -0700

    utils: Amend excerpt to handle emojis

--- packages/utils/src/excerpt.ts
+++ packages/utils/src/excerpt.ts
@@ -1,14 +1,18 @@
+const fullWidthRegex = /[^\u0020-\u007E]/;
+
 // Half-width characters have a width of 1, while full-width characters, like those in Japanese and Chinese, have a width of 2.
 function truncate(str: string, len: number): string {
   let truncated = "";
   let width = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    width += char.match(/[ -~]/) ? 1 : 2;
+  // eslint-disable-next-line no-restricted-syntax
+  for (const char of str) {
+    width += fullWidthRegex.test(char) ? 2 : 1;
+
     if (width > len - 1) {
       return `${truncated}…`;
     }
+
     truncated += char;
   }
 
