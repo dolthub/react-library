22916e068027c23467f8ec3f1fa0ab75f3370e8b
LiuLiu <liuliu.developer@gmail.com>
2024-07-12 14:43:49 -0700

    no sourcemap on watch, add color to output, only publish when all bundles are built

--- packages/components/rollup.watch.mjs
+++ packages/components/rollup.watch.mjs
@@ -4,7 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import external from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import { exec } from "child_process";
-import {watch} from "rollup";
+import { watch } from "rollup";
 import { dts } from "rollup-plugin-dts";
 import packageJson from "./package.json" assert { type: "json" };
 
@@ -34,55 +34,73 @@ const watchOptions = [
       {
         file: packageJson.main,
         format: "cjs",
-        sourcemap: true,
+        sourcemap: false,
         name: "react-ts-lib",
       },
       {
         file: packageJson.module,
         format: "esm",
-        sourcemap: true,
+        sourcemap: false,
       },
     ],
     plugins: plugins,
- 
+    watch: {
+      include: "src/**",
+    },
   },
   {
     input: "./types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     external: [/\.css$/],
-    plugins:[dts()]
+    plugins: [dts()],
+    watch: {
+      include: "types/**",
+    },
   },
 ];
 
-const watcher =  watch(watchOptions);
+const watcher = watch(watchOptions);
+let pendingBundles = watchOptions.length;
+const green = `\x1b[32m%s\x1b[0m`;
+const yellow = `\x1b[33m%s\x1b[0m`;
+const red = `\x1b[31m%s\x1b[0m`;
+
 watcher.on("event", event => {
   if (event.code === "ERROR") {
     console.error(event.error);
   }
-  if (event.code === "START") {
-    exec("yarn compile", (err,stderr) => {
+  if (event.code === "START" && pendingBundles === watchOptions.length) {
+    console.log(yellow, "Build started...");
+    exec("yarn compile", (err, stderr) => {
       if (err) {
-        console.error(`compile error: ${err}`);
+        console.error(red, `Compile error: ${err}`);
         return;
       }
       if (stderr) {
-        console.error(`compile stderr: ${stderr}`);
+        console.error(red, `Compile stderr: ${stderr}`);
       }
-      console.log(`compile completed`);
+      console.log(green, `Compile completed`);
     });
   }
   if (event.code === "BUNDLE_END") {
-    console.log("Build completed successfully");
-    exec("yalc publish", (err, stdout, stderr) => {
-      if (err) {
-        console.error(`Publish error: ${err}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`Publish stderr: ${stderr}`);
-      }
-      console.log(`Publish completed: ${stdout}`);
-    });
+    pendingBundles--;
+    console.log(green, "Build completed successfully for one bundle");
+
+    if (pendingBundles === 0) {
+      console.log(yellow, "All bundles built. Publishing...");
+
+      exec("yalc publish", (err, stdout, stderr) => {
+        if (err) {
+          console.error(red, `Publish error: ${err}`);
+          return;
+        }
+        if (stderr) {
+          console.error(red, `Publish stderr: ${stderr}`);
+        }
+        console.log(green, `Publish completed: ${stdout}`);
+        pendingBundles = watchOptions.length;
+      });
+    }
   }
 });
 
