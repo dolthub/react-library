cce04e38840a60b6b18d95335066e15532bd1de4
Taylor Bantle <taylor@dolthub.com>
2026-03-17 12:06:52 -0700

    resource-utils: Readd branch test export

--- packages/resource-utils/src/index.ts
+++ packages/resource-utils/src/index.ts
@@ -1,11 +1,8 @@
-export { ResourceNameConverter, ToResourceOptions } from "./ResourceNamesUtils";
 export {
-  Comparators,
-  NamedResource,
-  ResourceUtils,
-  Sorter,
-  Sorters,
-} from "./ResourceUtils";
+  invalidBranchNames,
+  invalidBranchProtectionNames,
+  validBranchNames
+} from "./__tests__/helpers/branchNames";
 export { default as byHash } from "./byHash";
 export { default as byName } from "./byName";
 export { default as byResourceName } from "./byResourceName";
@@ -13,10 +10,19 @@ export { default as comparatorsToSorters } from "./comparatorsToSorters";
 export { default as displayName } from "./displayName";
 export {
   default as doNameValidation,
-  ResourceNameValidationError,
+  ResourceNameValidationError
 } from "./doNameValidation";
 export {
-  ResourceParamValidationError,
   default as doParamValidation,
+  ResourceParamValidationError
 } from "./doParamValidation";
+export { ResourceNameConverter, ToResourceOptions } from "./ResourceNamesUtils";
+export {
+  Comparators,
+  NamedResource,
+  ResourceUtils,
+  Sorter,
+  Sorters
+} from "./ResourceUtils";
 export * as validResourceSegmentPattern from "./validResourceSegmentPattern";
+
